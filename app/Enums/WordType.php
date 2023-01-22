be6cb0505475453a8c462001e9e1dc6b633f3beb
Diego Barreiro Indart <diego.bindart@gmail.com>
2023-01-22 03:18:32 +0100

    New Enum WordType

---
+++ app/Enums/WordType.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Enums;
+
+enum WordType: string
+{
+    case Adjective    = 'adjective';
+
+    case Adverb       = 'adverb';
+
+    case Article      = 'article';
+
+    case Conjunction  = 'conjunction';
+
+    case Expression   = 'expression';
+
+    case Interjection = 'interjection';
+    
+    case Noun         = 'noun';
+    
+    case Preposition  = 'preposition';
+
+    case Pronoun      = 'pronoun';
+
+    case Verb         = 'verb';
+
+    case Unknown      = 'unknown';
+}
