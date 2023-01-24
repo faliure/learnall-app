6c197c8d3588e690dc800af68c7a0b58f77ac7e2
Diego Barreiro Indart <diego.bindart@gmail.com>
2023-01-24 01:50:15 +0100

    Improve regular Seeders

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,21 +2,16 @@
 
 namespace Database\Seeders;
 
-use App\Models\Sentence;
-use App\Models\Word;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
 {
-    /**
-     * Seed the application's database.
-     *
-     * @return void
-     */
     public function run()
     {
-        Sentence::factory(10)->create();
-
-        Word::factory(10)->create();
+        $this->call([
+            CategorySeeder::class,
+            WordSeeder::class,
+            SentenceSeeder::class,
+        ]);
     }
 }
