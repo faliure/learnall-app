fe56f62fd384718ce7e9f48b7803623a6a222d0e
Diego Barreiro Indart <diego.bindart@gmail.com>
2023-01-24 18:57:11 +0100

    New UserSeeder

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Seeder;
+
+class UserSeeder extends Seeder
+{
+    public function run()
+    {
+        if (User::whereName('Luke Skywalker')->doesntExist()) {
+            User::factory()->unverified()->create([
+                'name'  => 'Luke Skywalker',
+                'email' => 'luke@jedi.com',
+            ]);
+        }
+    }
+}
