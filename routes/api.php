bdad53690548a2c2ee56110ba91259bf806f230a
Diego Barreiro Indart <diego.bindart@gmail.com>
2023-01-24 19:45:53 +0100

    Implement Token auth with Sanctum

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,9 @@
 <?php
 
+use App\Http\Controllers\Auth\TokenController;
+use App\Http\Resources\UserResource;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,6 +17,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+Route::post('/auth/sanctum', TokenController::class);
+
+Route::middleware('auth:sanctum')->group(function () {
+    Route::get('/auth', fn () => new UserResource(Auth::user()));
 });
