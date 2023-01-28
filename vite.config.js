412643fb87e2d8151bdc0e629519b7931ea7cdb6
Diego Barreiro Indart <diego.bindart@gmail.com>
2023-01-28 01:28:06 +0100

    Fix vite HTTPS issues

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,9 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 import vue from '@vitejs/plugin-vue';
+import fs from 'fs';
+import { homedir } from 'os';
+import { resolve } from 'path';
 
 export default defineConfig({
     plugins: [
@@ -17,4 +20,28 @@ export default defineConfig({
             },
         }),
     ],
+
+    server: detectServerConfig('learnall.test'),
 });
+
+function detectServerConfig(host) {
+    let keyPath = resolve(homedir(), `.config/valet/Certificates/${host}.key`)
+    let certificatePath = resolve(homedir(), `.config/valet/Certificates/${host}.crt`)
+
+    if (!fs.existsSync(keyPath)) {
+        return {}
+    }
+
+    if (!fs.existsSync(certificatePath)) {
+        return {}
+    }
+
+    return {
+        hmr: {host},
+        host,
+        https: {
+            key: fs.readFileSync(keyPath),
+            cert: fs.readFileSync(certificatePath),
+        },
+    }
+}
