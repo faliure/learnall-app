55e59cbef49777990a5c9d80d46b59d1c3474915
Diego Barreiro Indart <diego.bindart@gmail.com>
2023-01-28 02:46:31 +0100

    Install Pinia Vue3 Store

--- resources/js/app.js
+++ resources/js/app.js
@@ -3,6 +3,7 @@ import '../css/app.css';
 
 import { createApp, h } from 'vue';
 import { createInertiaApp } from '@inertiajs/vue3';
+import pinia from '@/Store';
 import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
 import { ZiggyVue } from '../../vendor/tightenco/ziggy/dist/vue.m';
 
@@ -14,6 +15,7 @@ createInertiaApp({
     setup({ el, App, props, plugin }) {
         return createApp({ render: () => h(App, props) })
             .use(plugin)
+            .use(pinia)
             .use(ZiggyVue, Ziggy)
             .mount(el);
     },
