a036a9616c8e36ae42dc40fe95a04ae05061856c
Diego Barreiro Indart <diego.bindart@gmail.com>
2023-01-29 04:29:58 +0100

    Categories missing artifacts

---
+++ app/Http/Controllers/CategoryController.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace App\Http\Controllers\CrudActions;
+
+use App\Extensions\Laravel\CrudController;
+use App\Http\Requests\StoreCategoryRequest;
+use App\Http\Requests\UpdateCategoryRequest;
+use App\Http\Resources\CategoryResource;
+use App\Models\Category;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Resources\Json\ResourceCollection;
+use Illuminate\Http\Response;
+
+class CategoryController extends CrudController
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index(): ResourceCollection
+    {
+        return Category::resources();
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreCategoryRequest $request): CategoryResource
+    {
+        $category = Category::create($request->validated());
+
+        return $this->show($category);
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Category $category): CategoryResource
+    {
+        return $category->resource();
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateCategoryRequest $request, Category $category): CategoryResource
+    {
+        $category->update($request->validated());
+
+        return $this->show($category);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Category $category): JsonResponse
+    {
+        $category->delete();
+
+        return response()->json([], Response::HTTP_NO_CONTENT);
+    }
+}
---
+++ app/Http/Requests/StoreCategoryRequest.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class StoreCategoryRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return my('id') === 1; // TODO : create Roles, update this
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     */
+    public function rules(): array
+    {
+        return [
+            'name' => 'required|string|min:1|max:255|unique:categories',
+            'slug' => 'alpha_dash|min:3|max:255|unique:categories',
+        ];
+    }
+}
