d211ffea9d674acdbb2fe3f92b268b008a9ad7c0
Diego Barreiro Indart <diego.bindart@gmail.com>
2023-02-08 15:57:25 +0100

    Update NPM dependencies

--- resources/js/Layouts/TheHighlights.vue
+++ resources/js/Layouts/TheHighlights.vue
@@ -1,6 +1,6 @@
 <template>
   <section class="text-gray-300 text-center uppercase">
-    <div class="flex flex-col justify-center gap-6 h-1/2 rounded-xl border border-gray-200">
+    <div class="flex flex-col justify-center gap-6 h-1/2">
       <div class="text-2xl">Right Sidebar</div>
       <div class="text-base">(reserved)</div>
     </div>
