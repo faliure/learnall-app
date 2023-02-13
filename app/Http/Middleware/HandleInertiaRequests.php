969d0586e6b073d46c787b76c0767738b47f9b88
Diego Barreiro Indart <diego.bindart@gmail.com>
2023-02-13 15:27:01 +0100

    Fix crash for guests

--- app/Http/Middleware/HandleInertiaRequests.php
+++ app/Http/Middleware/HandleInertiaRequests.php
@@ -21,7 +21,7 @@ class HandleInertiaRequests extends Middleware
     public function share(Request $request): array
     {
         return array_merge(parent::share($request), [
-            'user'     => [
+            'user'     => auth()->guest() ? null : [
                 'id'        => me()->id,
                 'name'      => me()->name,
                 'email'     => me()->email,
